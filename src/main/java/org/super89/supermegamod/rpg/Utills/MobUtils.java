9b600f1a4987b853b47c8c62b0d41803cebeafdf
Nikita <nik.kiselev.2010@inbox.ru>
2024-01-21 22:25:05 +0400

    тест

--- src/main/java/org/super89/supermegamod/rpg/Utills/MobUtils.java
+++ src/main/java/org/super89/supermegamod/rpg/Utills/MobUtils.java
@@ -1,12 +1,14 @@
 package org.super89.supermegamod.rpg.Utills;
 
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.*;
 
 public class MobUtils {
-    public Zombie createZombie(Player p){
-        World w = p.getWorld();
-        Zombie zombie = (Zombie)
+    public Zombie createZombie(Player player, double x, double y, double z){
+        World world = player.getWorld();
+        Location location = new Location(world, x,y,z);
+        Zombie zombie = (Zombie) world.spawnEntity(location)
 
         return LivingEntity;
     }
