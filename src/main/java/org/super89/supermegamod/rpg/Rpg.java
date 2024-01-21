5c4be69a7919d5511388d5c886c9c070d7efd707
Super89123 <tedswa@mail.ru>
2024-01-21 21:25:12 +0400

    jj

---
+++ src/main/java/org/super89/supermegamod/rpg/Rpg.java
@@ -0,0 +1,17 @@
+package org.super89.supermegamod.rpg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Rpg extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
